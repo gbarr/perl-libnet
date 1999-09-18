140741a7fc1fd5af21bb93b060fcfd277f663035
Graham Barr <gbarr@pobox.com>
1999-09-18 16:07:15 +0000

    Net::Cmd - Map \n's in commands to " "

--- Net/Cmd.pm
+++ Net/Cmd.pm
@@ -13,7 +13,7 @@ use strict;
 use vars qw(@ISA @EXPORT $VERSION);
 use Carp;
 
-$VERSION = "2.14";
+$VERSION = "2.15";
 @ISA     = qw(Exporter);
 @EXPORT  = qw(CMD_INFO CMD_OK CMD_MORE CMD_REJECT CMD_ERROR CMD_PENDING);
 
@@ -168,7 +168,7 @@ sub command
   {
    local $SIG{PIPE} = 'IGNORE';
 
-   my $str =  join(" ",@_) . "\015\012";
+   my $str =  join(" ", map { tr/\n/ /; $_; } @_) . "\015\012";
    my $len = length $str;
    my $swlen;
    
