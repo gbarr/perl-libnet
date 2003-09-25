3a232eab88d61a03e37a92da96213fb763589b0c
Graham Barr <gbarr@pobox.com>
2003-09-25 09:22:26 +0000

    Net::FTP - Allow spaces after the file size in the response to SIZE

--- Net/FTP.pm
+++ Net/FTP.pm
@@ -22,7 +22,7 @@ use Net::Config;
 use Fcntl qw(O_WRONLY O_RDONLY O_APPEND O_CREAT O_TRUNC);
 # use AutoLoader qw(AUTOLOAD);
 
-$VERSION = "2.71"; # $Id: //depot/libnet/Net/FTP.pm#78 $
+$VERSION = "2.72"; # $Id: //depot/libnet/Net/FTP.pm#79 $
 @ISA     = qw(Exporter Net::Cmd IO::Socket::INET);
 
 # Someday I will "use constant", when I am not bothered to much about
@@ -206,7 +206,7 @@ sub size {
   my $io;
   if($ftp->supported("SIZE")) {
     return $ftp->_SIZE($file)
-	? ($ftp->message =~ /(\d+)$/)[0]
+	? ($ftp->message =~ /(\d+)\s*$/)[0]
 	: undef;
  }
  elsif($ftp->supported("STAT")) {
@@ -1767,6 +1767,6 @@ under the same terms as Perl itself.
 
 =for html <hr>
 
-I<$Id: //depot/libnet/Net/FTP.pm#78 $>
+I<$Id: //depot/libnet/Net/FTP.pm#79 $>
 
 =cut
