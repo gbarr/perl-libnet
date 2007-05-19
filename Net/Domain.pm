5fd1783c84c84a7b01a29f283cd875b333d9014b
Graham Barr <gbarr@pobox.com>
2007-05-19 13:35:22 +0000

    Version update

--- Net/Domain.pm
+++ Net/Domain.pm
@@ -16,7 +16,7 @@ use Net::Config;
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(hostname hostdomain hostfqdn domainname);
 
-$VERSION = "2.19_01";
+$VERSION = "2.20";
 
 my ($host, $domain, $fqdn) = (undef, undef, undef);
 
