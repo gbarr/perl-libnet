2c6a5afd1ef525c30ad32427f4dd20bc6e819403
Steve Hay <steve.m.hay@googlemail.com>
2014-01-29 08:35:24 +0000

    Bump $Net::Domain::VERSION for previous change

--- Net/Domain.pm
+++ Net/Domain.pm
@@ -16,7 +16,7 @@ use Net::Config;
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(hostname hostdomain hostfqdn domainname);
 
-$VERSION = "2.22";
+$VERSION = "2.23";
 
 my ($host, $domain, $fqdn) = (undef, undef, undef);
 
